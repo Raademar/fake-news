513624f3f4de262e4f5450c617ff4a8599696e2b
Mattias Rådemar <mattiasrademar@Mattiass-MacBook-Pro.local>
2018-10-17 10:13:52 +0200

    adding materializecss and mergeing with init commit.

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
 
-require(__DIR__. 'data/functions.php');
-require(__DIR__. 'data/data.php');
+require(__DIR__. '/data/functions.php');
+require(__DIR__. '/data/data.php');
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <title>Fake News</title>
+</head>
+<body>
+
+  
+
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>

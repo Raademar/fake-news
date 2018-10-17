a7ae3801db47211aa2caa228475b1391a99a933f
Mattias Rådemar <mattiasrademar@Mattiass-MacBook-Pro.local>
2018-10-17 13:44:41 +0200

    added database, brought in materialize. basic database setup complete, started adding new table for Users.

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
+declare(strict_types=1);
 
 require(__DIR__. '/data/functions.php');
 require(__DIR__. '/data/data.php');
+require(__DIR__. '/data/db.php');
+
+//Query the database
+$sqlite = 'SELECT * FROM Articles ORDER BY Dt Desc';
+$result = $db->query($sqlite);
+
+// Close the connection
+$db = NULL;
 
 ?>
 
@@ -11,14 +20,30 @@ require(__DIR__. '/data/data.php');
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link rel="stylesheet" href="assets/main.css">
   <title>Fake News</title>
 </head>
 <body>
-
-  
-
-
+  <nav>
+    <div class="nav-wrapper blue darken-1 ml-4">
+      <a href="#" class="brand-logo">Freaky Friday News</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container">
+    <?php foreach($result as $row): ?>
+      <h3><?=$row['Title'];?></h3>
+      <p><?=$row['Body'];?></p>
+      <p><?=$row['Author'];?></p>
+      <span class="badge"><?=$row['Likes'];?><i class="tiny material-icons like-button">thumb_up</i></span>
+      <p><?=$row['Dt'];?></p>
+    <?php endforeach; ?>
+  </div>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 </html>

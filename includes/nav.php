82f210e0d9486584855fd2f72d6af0d0e1096d2b
Mattias Rådemar <mattiasrademar@Mattiass-MacBook-Pro.local>
2018-10-31 17:20:13 +0100

    had to add a quick mediaquery for the navbar.

--- includes/nav.php
+++ includes/nav.php
@@ -1,8 +1,8 @@
 <body>
   <nav>
     <div class="nav-wrapper blue darken-4 ml-4 nav-text">
-      <a href="index.php" class="brand-logo nav-margin nav-text">Freaky Friday News</a>
-      <ul id="nav-mobile" class="right nav-margin">
+      <a href="index.php" class="brand-logo navlogo-margin nav-text">FFN</a>
+      <ul id="nav-mobile" class="nav-mobile hide-on-med-and-down right nav-margin">
         <li>
           <a href="new-post.php">New Post</a>
         </li>
